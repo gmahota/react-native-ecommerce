c88a0c1fda7948b220d4ce32b1e4803767f2c751
Guimarães Mahota <guimaraesmahota@gmail.com>
2022-02-07 21:57:49 +0200

    add card list

--- screens/TabHomeScreen.tsx
+++ screens/TabHomeScreen.tsx
@@ -1,28 +1,110 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
-import { StyleSheet } from 'react-native';
+import { StyleSheet, SafeAreaView, ScrollView, StatusBar } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 import { useWindowDimensions } from 'react-native';
-import {TabBar , TabView, SceneMap } from 'react-native-tab-view';
+import { TabBar, TabView, SceneMap } from 'react-native-tab-view';
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+
+
+const restaurantsP: any = [{
+  name: 'Restaurant 1',
+  address: 'Maputo, Rua Dr. Jaime Ribeiro Nº 10',
+  phone: '123456789',
+  image: 'https://picsum.photos/201',
+
+},
+{
+  name: 'Restaurant 2',
+  address: 'Maputo, Rua Dr. Jaime Ribeiro Nº 10',
+  phone: '123456789',
+  image: 'https://picsum.photos/202',
+
+},
+{
+  name: 'Restaurant 3',
+  address: 'Maputo, Rua Dr. Jaime Ribeiro Nº 10',
+  phone: '123456789',
+  image: 'https://picsum.photos/203',
+
+}]
+
+const restaurantsD: any = [{
+  name: 'Restaurant 1',
+  address: 'Maputo, Rua Dr. Jaime Ribeiro Nº 10',
+  phone: '123456789',
+  image: 'https://picsum.photos/700',
+
+},
+{
+  name: 'Restaurant 2',
+  address: 'Maputo, Rua Dr. Jaime Ribeiro Nº 10',
+  phone: '123456789',
+  image: 'https://picsum.photos/709',
+
+},
+{
+  name: 'Restaurant 3',
+  address: 'Maputo, Rua Dr. Jaime Ribeiro Nº 10',
+  phone: '123456789',
+  image: 'https://picsum.photos/701',
+
+}, {
+  name: 'Restaurant 4',
+  address: 'Maputo, Rua Dr. Jaime Ribeiro Nº 10',
+  phone: '123456789',
+  image: 'https://picsum.photos/703',
+
+}]
+
+const LeftContent = (props: any) => <Avatar.Icon {...props} icon="folder" />
 
 const DeliveryRoute = () => (
-  <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+  <SafeAreaView style={styles.container}>
+    <ScrollView >
+
+      <Text style={styles.title}>Order's from {restaurantsD.length} restaurants</Text>
+      {
+        restaurantsD.map((restaurant: any) => (
+          <Card key={restaurant.name} style={{ marginTop: 10, marginLeft:10,marginRight:10 }}>
+            <Card.Cover source={{ uri: restaurant.image }} />
+
+            <Card.Content>
+              <Title>{restaurant.name}</Title>
+              <Paragraph>{restaurant.address}</Paragraph>
+            </Card.Content>
+
+          </Card>
+        ))
+      }
+    </ScrollView>
+  </SafeAreaView>
 );
 
 const PickupRoute = () => (
-  <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+
+  <SafeAreaView style={styles.container}>
+    <ScrollView >
+      <Text style={styles.title}>Order's from {restaurantsP.length} restaurants</Text>
+      {
+        restaurantsP.map((restaurant: any) => (
+          <Card key={restaurant.name} style={{ marginTop: 10, marginLeft:10,marginRight:10 }}>
+            <Card.Cover source={{ uri: restaurant.image }} />
+
+            <Card.Content>
+              <Title>{restaurant.name}</Title>
+              <Paragraph>{restaurant.address}</Paragraph>
+            </Card.Content>
+
+          </Card>
+        ))
+      }
+    </ScrollView>
+  </SafeAreaView>
   //<View style={{ flex: 1, backgroundColor: '#673ab7' }} />
 );
 
@@ -31,12 +113,12 @@ const renderScene = SceneMap({
   pickup: PickupRoute,
 });
 
-const renderTabBar = (props:any) => (
+const renderTabBar = (props: any) => (
   <TabBar
     {...props}
     indicatorStyle={{ backgroundColor: '#e65100' }}
     style={{ backgroundColor: 'white' }}
-    labelStyle={{ color: '#e65100'}}
+    labelStyle={{ color: '#e65100' }}
   />
 );
 
@@ -50,7 +132,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabHome
   ]);
 
   return (
-<TabView
+    <TabView
       navigationState={{ index, routes }}
       renderScene={renderScene}
       onIndexChange={setIndex}
@@ -59,18 +141,18 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabHome
     />
 
 
-    
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
   title: {
-    fontSize: 20,
+    fontSize: 10,
     fontWeight: 'bold',
   },
   separator: {
